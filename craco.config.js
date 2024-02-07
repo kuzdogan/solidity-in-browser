c9d4e62602d0b9d292bc39a600adcd6507669fe4
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-02-07 08:21:25 +0100

    Resolve polyfills

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,18 @@
 // const webpack = require("webpack");
 module.exports = {
-  webpack: {},
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback, // if you have existing fallbacks
+        buffer: require.resolve("buffer"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        stream: require.resolve("stream-browserify"),
+        url: false,
+        assert: require.resolve("assert"),
+        util: require.resolve("util"),
+      };
+      return webpackConfig;
+    },
+  },
 };
